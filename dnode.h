3fd4628dfb0326a423711a16c65ef6aa13557a7b
Eduardo48 <63999023+Eduardo48@users.noreply.github.com>
2020-04-21 22:02:58 -0500

    dnode.h

---
+++ dnode.h
@@ -0,0 +1,17 @@
+#ifndef DNODE_H
+#define DNODE_H
+
+typedef int DATA;
+
+typedef struct _node node;
+
+struct _node{
+    DATA data;
+    node *next;
+};
+
+node *create_node(DATA data);
+void delete_node(node *n);
+
+#endif
+
